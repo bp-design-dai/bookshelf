89afaaa14a04a83eddd5f454375aa006d072c9e1
bp-design <info@bp-design.site>
2026-06-30 16:10:40 +0900

    fix: verify fortify authentication flow

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,1 +1,10 @@
 <h1>Login</h1>
+
+<form method="POST" action="/login">
+    @csrf
+
+    <input type="email" name="email" placeholder="メールアドレス">
+    <input type="password" name="password" placeholder="パスワード">
+
+    <button type="submit">ログイン</button>
+</form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,12 @@
 <h1>Register</h1>
+
+<form method="POST" action="/register">
+    @csrf
+
+    <input type="text" name="name" placeholder="名前">
+    <input type="email" name="email" placeholder="メールアドレス">
+    <input type="password" name="password" placeholder="パスワード">
+    <input type="password" name="password_confirmation" placeholder="パスワード確認">
+
+    <button type="submit">登録</button>
+</form>
